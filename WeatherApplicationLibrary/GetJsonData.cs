a192c29aa1559f9aeb517316c26f4407da910b6d
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2023-07-26 09:53:56 +0200

    Added GetJsonData class to download JSON files

---
+++ WeatherApplicationLibrary/GetJsonData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplicationLibrary
+{
+    public class GetJsonData
+    {
+        public string jsonStr { get; init; }
+        public GetJsonData(string urlAdress)
+        {
+            using (WebClient client = new WebClient())
+            {
+                jsonStr = client.DownloadString(urlAdress);
+            }
+        }
+    }
+}
