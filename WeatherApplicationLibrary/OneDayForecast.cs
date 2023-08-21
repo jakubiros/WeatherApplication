554779bcfecad9699eccead733bda7fa08dd1db9
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2023-08-21 17:59:53 +0200

    Added dictionary with forecast data

--- WeatherApplicationLibrary/OneDayForecast.cs
+++ WeatherApplicationLibrary/OneDayForecast.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,10 @@ namespace WeatherApplicationLibrary
     {
         private WeatherData data;
         private int numOfDays => data.hourly.time.Length/24;
-        public Dictionary<TimeOnly,Dictionary<List<string>,Array>> hourlyDayForecast { get; private set; }
+        private List<Dictionary<string,List<Dictionary<string, string>>>> hourlyDayForecastList = new List<Dictionary<string,List<Dictionary<string, string>>>>();
+        //public Dictionary<TimeOnly,Dictionary<List<string>,Array>> hourlyDayForecast { get; private set; }
         private List<string> hourlyAttributes = new List<string>();
+        private List<string[]> forecastValues = new List<string[]>();
 
         public OneDayForecast(WeatherData forecast, int dayOfForecast)
         {
@@ -30,21 +33,41 @@ namespace WeatherApplicationLibrary
             else if (dayOfForecast == 2) startInd = 47;
 
             string[] time = data.hourly.time;
-            float[] temperature_2m = data.hourly.temperature_2m;
-            int[] relativehumidity_2m = data.hourly.relativehumidity_2m;
-            float[] apparent_temperature = data.hourly.apparent_temperature;
-            int[] precipitation_probability = data.hourly.precipitation_probability;
-            float[] pressure_msl = data.hourly.pressure_msl;
-            int[] cloudcover = data.hourly.cloudcover;
-            float[] visibility = data.hourly.visibility;
-            float[] windspeed_10m = data.hourly.windspeed_10m;
-            int[] winddirection_10m = data.hourly.winddirection_10m;
+            string[] temperature_2m = data.hourly.temperature_2m.Select(x=>x.ToString()).ToArray();
+            string[] relativehumidity_2m = data.hourly.relativehumidity_2m.Select(x => x.ToString()).ToArray();
+            string[] apparent_temperature = data.hourly.apparent_temperature.Select(x => x.ToString()).ToArray();
+            string[] precipitation_probability = data.hourly.precipitation_probability.Select(x => x.ToString()).ToArray();
+            string[] pressure_msl = data.hourly.pressure_msl.Select(x => x.ToString()).ToArray();
+            string[] cloudcover = data.hourly.cloudcover.Select(x => x.ToString()).ToArray();
+            string[] visibility = data.hourly.visibility.Select(x => x.ToString()).ToArray();
+            string[] windspeed_10m = data.hourly.windspeed_10m.Select(x => x.ToString()).ToArray();
+            string[] winddirection_10m = data.hourly.winddirection_10m.Select(x => x.ToString()).ToArray(); 
+
+            forecastValues.Add(temperature_2m);
+            forecastValues.Add(relativehumidity_2m);
+            forecastValues.Add(apparent_temperature);
+            forecastValues.Add(precipitation_probability);
+            forecastValues.Add(pressure_msl);
+            forecastValues.Add(cloudcover);
+            forecastValues.Add(visibility);
+            forecastValues.Add(windspeed_10m);
+            forecastValues.Add(winddirection_10m);
 
             while (startInd == endInd)
             {
-
-                //hourlyDayForecast.Add(new Dictionary<TimeOnly, Dictionary<List<string>, Array>>(TimeOnly.Parse(data.hourly.time[startInd].Split("T")[1],
-                //    new Dictionary<List<string>, Array>(hourlyAttributes, ))
+                List<Dictionary<string,string>> dictList= new List<Dictionary<string,string>>();
+                for (int i =0; i<forecastValues.Count; i++)
+                {
+                    var d= new Dictionary<string,string>();
+                    var temp = forecastValues[i];
+                    d.Add(hourlyAttributes[i], temp[startInd]);
+                    dictList.Add(d);
+                }
+                //temp.Add(hourlyAttributes[startInd]);
+                //temp.Add(temperature_2m[startInd].ToString());
+                var tempDict = new Dictionary<string, List<Dictionary<string, string>>>();
+                tempDict.Add(time[startInd].Split("T")[1], dictList);
+                hourlyDayForecastList.Add(tempDict);
                 
             }
         }
